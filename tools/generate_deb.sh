ca2b09541b5b35bf366557beac982f301a7d3082
kazuto28 <konndoukazuto45@gmail.com>
2021-06-24 20:42:21 +0900

    generator update

--- tools/generate_deb.sh
+++ tools/generate_deb.sh
@@ -3,10 +3,11 @@ TGT_PKG="${TMP_DIR}output/"
 
 rm -rf ${TMP_DIR}
 mkdir ${TMP_DIR}
-wheel2deb -x dist/ --depends ffmpeg gettext python3-pubsub python3-twodict python3-wxgtk4.0 --name youtube-dlg -o ${TMP_DIR}output/
+python3 setup.py bdist_wheel
+wheel2deb dist/*.whl --depends ffmpeg gettext python3-pubsub python3-twodict python3-wxgtk4.0 --name youtube-dlg -o ${TMP_DIR}output/ -c tools/wheel2deb.yml
 TGT_PKG=${TGT_PKG}`ls ${TGT_PKG}`
 cp tools/youtube-dlg.desktop ${TGT_PKG}/src/
 echo -e "\nsrc/youtube-dlg.desktop /usr/share/applications/" >> ${TGT_PKG}/debian/install
-sed -e "s/wheel2deb <wheel2deb@upciti.com>/kazuto28 <mountaindull@gmail.com>/g" -i ${TGT_PKG}/debian/control -i ${TGT_PKG}/debian/changelog
 wheel2deb build -p ${TMP_DIR}output/
 cp ${TMP_DIR}output/*.deb packages/
+rm -rf ${TMP_DIR}
